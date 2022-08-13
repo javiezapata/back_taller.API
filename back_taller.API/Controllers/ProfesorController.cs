da4c9dc86c5d8bfcb9fb6f588975466bba13ffe0
jzapata <jzapata@L111>
2022-08-12 21:03:30 -0500

    Add project files.

---
+++ back_taller.API/Controllers/ProfesorController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+
+
+
+namespace back_taller.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProfesorController : ControllerBase
+    {
+
+        [HttpGet(Name = "GetProfesor")]
+        public ActionResult GetProfesor(string name)
+        {
+            return  Ok(new
+            {
+                hola = $"Hola mundo, {name}"
+            });
+        }
+    }
+}
